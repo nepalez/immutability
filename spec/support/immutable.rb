e82f644240112b1951d0bd88da87e48e343c1add
Andrew Kozin <andrew.kozin@gmail.com>
2015-09-20 17:13:16 +0300

    [BUG FIX] Fixed bug in rspec matcher

--- spec/support/immutable.rb
+++ spec/support/immutable.rb
@@ -9,31 +9,17 @@
 #
 RSpec::Matchers.define :be_immutable do
   match do |instance|
-    expect(instance).to be_frozen
-
-    ivars = instance.instance_variables
-    ivars.each { |ivar| expect(ivar).to be_immutable_part_of instance }
+    if instance.class.respond_to? :new
+      expect(instance).to be_frozen
+      instance
+        .instance_variables.map(&instance.method(:instance_variable_get))
+        .each { |ivar| expect(ivar).to be_immutable }
+    else
+      expect(true).to be_truthy
+    end
   end
 
   failure_message do |instance|
     "expected that #{instance.inspect} to be immutable"
-  end
-end
-
-# Checks whether the variable is a truly immutable part of given instance
-#
-# @api private
-#
-RSpec::Matchers.define :be_immutable_part_of do |instance|
-  match do |ivar|
-    expect(ivar).to be_frozen
-    ivar.instance_variables.each do |var|
-      expect(var).to be_immutable_part_of instance
-    end
-  end
-
-  failure_message do |ivar|
-    object = instance.inspect
-    "expected that #{ivar.inspect} to be frozen part of immutable #{object}"
   end
 end
